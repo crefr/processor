5f86d17d65818dc55150467a918266fd72f775d4
crefr <fedor30122005@gmail.com>
2024-10-09 22:20:28 +0300

    added assembler

---
+++ comands.h
@@ -0,0 +1,25 @@
+#ifndef COMANDS_INCLUDED
+#define COMANDS_INCLUDED
+
+enum commands
+{
+    PUSH_CMD = 1,
+
+    ADD_CMD,
+    SUB_CMD,
+    MUL_CMD,
+    DIV_CMD,
+
+    OUT_CMD,
+    IN_CMD,
+
+    SQRT_CMD,
+    SIN_CMD,
+    COS_CMD,
+
+    DUMP_CMD,
+
+    HLT_CMD = 0
+};
+
+#endif
